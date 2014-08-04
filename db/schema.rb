d21c05eb6bc6c623c8f7ed79d78891ec49bbe47e
Stephania Gambaroff Thomas <stgambar@gambaroff.com>
2014-08-03 20:14:56 -0700

    Link league model to the event model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140803072448) do
+ActiveRecord::Schema.define(version: 20140803213821) do
 
   create_table "events", force: true do |t|
     t.string   "name"
