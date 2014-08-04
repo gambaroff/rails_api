a099d4b61fc2a7496e02f162c3d802f55df4e081
Stephania Gambaroff Thomas <stgambar@gambaroff.com>
2014-08-03 21:45:17 -0700

    Add the show action, so we can look up the league specifiend in the params

--- app/controllers/leagues_controller.rb
+++ app/controllers/leagues_controller.rb
@@ -3,6 +3,10 @@ class LeaguesController < ApplicationController
 		render json: League.all
 	end	
 
+	def show
+		render json: League.find(params[:id])
+	end
+
 	private
 
 	def default_serializer_options
